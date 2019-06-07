9d0c2408415187a65879a89761eb0629b8f0211d
Tara Dias <diastara01@gmail.com>
2019-06-07 15:01:05 +1000

    hate life rn

---
+++ evolution/assets/java/popup-1.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+
+
+// 25k popup    
+$('.l-MS').click(function() {
+  $('.p-MS').addClass('show');
+});
+
+$('.p-MS').click(function() {
+  $('.p-MS').removeClass('show');
+});
+
+// 35k popup
+$('.l-30k').click(function() {
+  $('.p-30k').addClass('show');
+});
+
+$('.p-30k').click(function() {
+  $('.p-30k').removeClass('show');
+});
+
+// 45k popup
+$('.l-DM').click(function() {
+  $('.p-DM').addClass('show');
+});
+
+$('.p-DM').click(function() {
+  $('.p-DM').removeClass('show');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
