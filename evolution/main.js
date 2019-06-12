e7dab42758d6c39a260e6df7c17928aa9f4656a8
Tara Dias <diastara01@gmail.com>
2019-06-12 19:23:36 +1000

    rfrfrw

--- evolution/main.js
+++ evolution/main.js
@@ -1,38 +1,4 @@
-$( document ).ready(function() {
-// start here 
-$('.menu-link').click(function() {
-  $('.menu').addClass('left');
-});
-
-$('.close-link').click(function() {
-  $('.menu').removeClass('left');
-});  
-
-// popup link
-
-$('.popup-link').click(function() {
-  $('.popup').addClass('active');
-}); 
-
-$('.popup').click(function() {
-  $('.popup').removeClass('active');
-});  
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// end here
-});
+function showIt() {  
+  document.getElementById("hide").style.visibility = "visible";  
+}  
+setTimeout("showIt()", 4000);  
